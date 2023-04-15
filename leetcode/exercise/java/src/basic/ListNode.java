a0565e5d90c0ed136e6acd897e758c7f437bdea0
Xinwu Cheng <xinwucheng@gmail.com>
2023-04-15 10:27:56 -0700

    Fix missing commits

--- leetcode/exercise/java/src/basic/ListNode.java
+++ leetcode/exercise/java/src/basic/ListNode.java
@@ -7,4 +7,17 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    public static ListNode deserialize(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return null;
+
+        ListNode head = new ListNode(arr[0]);
+        ListNode node = head;
+        for (int i = 1; i < arr.length; ++i) {
+            node.next = new ListNode(arr[i]);
+            node = node.next;
+        }
+        return head;
+    }
 }
