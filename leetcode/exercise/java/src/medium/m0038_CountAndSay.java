bc853c8a2def4948846c2f1066ef32af548c3312
Xinwu Cheng <xinwucheng@gmail.com>
2023-09-16 10:00:52 -0700

    two medium easy questions

---
+++ leetcode/exercise/java/src/medium/m0038_CountAndSay.java
@@ -0,0 +1,77 @@
+package medium;
+
+/**
+ * 38. Count and Say
+ * https://leetcode.com/problems/count-and-say/description/
+ * 
+ * The count-and-say sequence is a sequence of digit strings defined by the
+ * recursive formula:
+ * 
+ * countAndSay(1) = "1"
+ * countAndSay(n) is the way you would "say" the digit string from
+ * countAndSay(n-1), which is then converted into a different digit string.
+ * To determine how you "say" a digit string, split it into the minimal number
+ * of substrings such that each substring contains exactly one unique digit.
+ * Then for each substring, say the number of digits, then say the digit.
+ * Finally, concatenate every said digit.
+ * 
+ * For example, the saying and conversion for digit string "3322251":
+ * 
+ * Given a positive integer n, return the nth term of the count-and-say
+ * sequence.
+ * 
+ * Example 1:
+ * 
+ * Input: n = 1
+ * Output: "1"
+ * Explanation: This is the base case.
+ * Example 2:
+ * 
+ * Input: n = 4
+ * Output: "1211"
+ * Explanation:
+ * countAndSay(1) = "1"
+ * countAndSay(2) = say "1" = one 1 = "11"
+ * countAndSay(3) = say "11" = two 1's = "21"
+ * countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+ * 
+ * Constraints:
+ * 
+ * 1 <= n <= 30
+ */
+public class m0038_CountAndSay {
+    static class Solution {
+        String[] results = new String[31];
+
+        public String countAndSay(int n) {
+            if (n == 1) {
+                results[n] = "1";
+            } else {
+                String prev = results[n - 1] == null ? countAndSay(n - 1) : results[n - 1];
+                StringBuilder sb = new StringBuilder();
+                int count = 1;
+                char digit = prev.charAt(0);
+                for (int i = 1; i < prev.length(); ++i) {
+                    if (digit == prev.charAt(i)) {
+                        count++;
+                    } else {
+                        sb.append(count);
+                        sb.append(digit);
+                        count = 1;
+                        digit = prev.charAt(i);
+                    }
+                }
+                sb.append(count);
+                sb.append(digit);
+                results[n] = sb.toString();
+            }
+            return results[n];
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        for (int i = 1; i <= 30; ++i)
+            System.out.format("%d: %s\n", i, solution.countAndSay(i));
+    }
+}
---
+++ leetcode/exercise/java/src/medium/m0443_StringCompression.java
@@ -0,0 +1,97 @@
+package medium;
+
+import java.util.Arrays;
+
+/**
+ * 443. String Compression
+ * https://leetcode.com/problems/string-compression/description/
+ * 
+ * Given an array of characters chars, compress it using the following
+ * algorithm:
+ * 
+ * Begin with an empty string s. For each group of consecutive repeating
+ * characters in chars:
+ * 
+ * If the group's length is 1, append the character to s.
+ * Otherwise, append the character followed by the group's length.
+ * The compressed string s should not be returned separately, but instead, be
+ * stored in the input character array chars. Note that group lengths that are
+ * 10 or longer will be split into multiple characters in chars.
+ * 
+ * After you are done modifying the input array, return the new length of the
+ * array.
+ * 
+ * You must write an algorithm that uses only constant extra space.
+ * 
+ * Example 1:
+ * 
+ * Input: chars = ["a","a","b","b","c","c","c"]
+ * Output: Return 6, and the first 6 characters of the input array should be:
+ * ["a","2","b","2","c","3"]
+ * Explanation: The groups are "aa", "bb", and "ccc". This compresses to
+ * "a2b2c3".
+ * Example 2:
+ * 
+ * Input: chars = ["a"]
+ * Output: Return 1, and the first character of the input array should be: ["a"]
+ * Explanation: The only group is "a", which remains uncompressed since it's a
+ * single character.
+ * Example 3:
+ * 
+ * Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+ * Output: Return 4, and the first 4 characters of the input array should be:
+ * ["a","b","1","2"].
+ * Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to
+ * "ab12".
+ * 
+ * Constraints:
+ * 
+ * 1 <= chars.length <= 2000
+ * chars[i] is a lowercase English letter, uppercase English letter, digit, or
+ * symbol.
+ */
+public class m0443_StringCompression {
+    static class Solution {
+        public int compress(char[] chars) {
+            int count = 1;
+            char currentChar = chars[0];
+            int len = 0;
+            for (int i = 1; i < chars.length; ++i) {
+                if (currentChar == chars[i]) {
+                    count++;
+                } else {
+                    chars[len++] = currentChar;
+                    if (count > 1)
+                        for (char c : String.valueOf(count).toCharArray())
+                            chars[len++] = c;
+                    count = 1;
+                    currentChar = chars[i];
+                }
+            }
+            chars[len++] = currentChar;
+            if (count > 1)
+                for (char c : String.valueOf(count).toCharArray())
+                    chars[len++] = c;
+            return len;
+        }
+    }
+
+    private static void test(char[] chars, char[] expects) {
+        Solution s = new Solution();
+        String input = new String(chars);
+        String expect = new String(expects);
+        int len = s.compress(chars);
+        String output = new String(chars, 0, len);
+        System.out.format("%s: %s => %s (vs) %s\n", output.equals(expect), input, output, expect);
+
+    }
+
+    public static void main(String[] args) {
+        test(new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' }, new char[] { 'a', '2', 'b', '2', 'c', '3' });
+        test(new char[] { 'a' }, new char[] { 'a' });
+        test(new char[] { 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b' },
+                new char[] { 'a', 'b', '1', '2' });
+        test(new char[] { 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'B', 'B' },
+                new char[] { 'a', 'b', '1', '0', 'B', '2' });
+    }
+}
